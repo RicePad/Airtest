c04d84df0c58937224baa85573d3dad5f5a31512
RiskyClick <jonathanli@wiredhere.com>
2017-03-16 18:31:06 +0000

    commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,9 @@ class ApplicationController < ActionController::Base
  before_action :configure_permitted_paramters, if: :devise_controller?
 
   protected
+  	
   	def configure_permitted_paramters
     	devise_parameter_sanitizer.permit(:sign_up, keys: [:fullname])
       devise_parameter_sanitizer.permit(:account_update, keys: [:fullname, :phone_number, :description, :email, :password])
     end
-end
+ end
